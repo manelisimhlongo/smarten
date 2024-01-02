d6dfe9ad790e58e6ed628eb92210fb866b527b98
manelisimhlongo <manelis@atlasdot.co.za>
2024-01-02 09:20:40 +0200

    banner and footer background image

--- src/containers/footer/footer.jsx
+++ src/containers/footer/footer.jsx
@@ -1,9 +1,27 @@
 import React from "react";
 import './footer.css';
+import backgroundImage from '../../assets/footer_image.jpeg';
+import star from '../../assets/star.svg';
+import rectangle from '../../assets/rectangle.png';
+
 
 const Footer = () => {
     return (
         <div className="footer-bg">
+
+        
+            <div className="banner-bar-row">
+
+                <div className="banner-bar-text">Game Spanning</div>
+
+                <img src={star} alt="star" />
+                <div className="banner-bar-text">Action - Packed</div>
+                <img src={star} alt="star" />
+                <div className="banner-bar-text">Mind Bending</div>
+                <img src={star} alt="star" />
+                <div className="banner-bar-text">collection og games</div>
+            </div>
+            <img src={backgroundImage} alt="img" />
 
         </div>
     );
