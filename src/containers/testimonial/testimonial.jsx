c129a7d36e219cbdb245e56fd537e185dd4a6136
manelisimhlongo <manelis@atlasdot.co.za>
2024-01-02 22:03:41 +0200

    testimonial page styling

--- src/containers/testimonial/testimonial.jsx
+++ src/containers/testimonial/testimonial.jsx
@@ -3,6 +3,8 @@ import './testimonial.css';
 import star from '../../assets/star.svg';
 import icons from '../../assets/icons.svg';
 import { TestimonialCard } from "../../components";
+import avatar1 from '../../assets/testimonial_1.png';
+import avatar2 from '../../assets/testimonial_2.png';
 
 
 const Testimonial = () => {
@@ -21,9 +23,9 @@ const Testimonial = () => {
             </div>
             <div className="space-height"></div>
             <div className="testimonial-row">
-                <TestimonialCard img={''} name={'Arlene McCoy'} subtext={"McDonald's"} text={'One of the standout features of this gaming website is its extensive library of game guides and tutorials. It has helped me level up my skills, conquer challenging quests, and discover hidden secrets within games. The guides are comprehensive, easy to follow, and have undoubtedly elevated my gaming performance.'} />
+                <TestimonialCard img={avatar1} name={'Arlene McCoy'} subtext={"McDonald's"} text={'One of the standout features of this gaming website is its extensive library of game guides and tutorials. It has helped me level up my skills, conquer challenging quests, and discover hidden secrets within games. The guides are comprehensive, easy to follow, and have undoubtedly elevated my gaming performance.'} />
                 <div className="space-width"></div>
-                <TestimonialCard img={''} name={'Kathryn Murphy'} subtext={"General Electric"} text={"Another aspect that sets this website apart is its vibrant and passionate community. The forum section provides a platform for gamers from all walks of life to connect, share their experiences, and discuss their favorite titles. I've made valuable friendships and found like-minded individuals who share my enthusiasm for gaming"} />
+                <TestimonialCard img={avatar2} name={'Kathryn Murphy'} subtext={"General Electric"} text={"Another aspect that sets this website apart is its vibrant and passionate community. The forum section provides a platform for gamers from all walks of life to connect, share their experiences, and discuss their favorite titles. I've made valuable friendships and found like-minded individuals who share my enthusiasm for gaming"} />
             </div>
             <div className="row">
                 <div className="bottom-icons">
