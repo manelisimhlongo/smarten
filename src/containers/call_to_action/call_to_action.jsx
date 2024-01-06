9e59a4f1f1de547eb9dbdb19ed08672fae50d79e
manelisimhlongo <manelis@atlasdot.co.za>
2024-01-06 23:30:56 +0200

    cta css file refactoring

--- src/containers/call_to_action/call_to_action.jsx
+++ src/containers/call_to_action/call_to_action.jsx
@@ -12,8 +12,8 @@ const Action = () => {
         <div className="cta-background">
             <div className="cta-column">
 
-                <div className="space-height"></div>
-                <div className="space-width"></div>
+                <div className="space-20h"></div>
+                <div className="space-20w"></div>
                 <div className="cta-row">
 
                     <div className="cta-container">
@@ -27,21 +27,21 @@ const Action = () => {
                                 </div>
 
                             </div>
-                            <div className="space-width"></div>
-                            <div className="space-width"></div>
-                            <div className="space-width"></div>
+                            <div className="space-20w"></div>
+                            <div className="space-20w"></div>
+                            <div className="space-20w"></div>
 
 
                             <div className="cta-column">
-                                <div className="space-height-10h"></div>
-                                <div className="space-height-10h"></div>
-                                <div className="space-height-10h"></div>
-                                <div className="space-height-10h"></div>
+                                <div className="space-10h"></div>
+                                <div className="space-10h"></div>
+                                <div className="space-10h"></div>
+                                <div className="space-10h"></div>
                                 <div className="cta-heading-container">
                                     <div className="cta-heading">discover the</div>
                                     <div className="cta-row-heading">
                                         <div className="cta-heading-gradient">virtual </div>
-                                        <div className="space-width-10"></div>
+                                        <div className="space-10w"></div>
                                         <div className="cta-heading">reality </div>
                                     </div>
                                     <div className="cta-heading">gaming </div>
@@ -52,10 +52,10 @@ const Action = () => {
                                         A well-designed gaming header often incorporates elements such as game characters, iconic symbols, vibrant colors, and dynamic visuals to convey excitement, adventure, and the immersive nature of gaming.
                                     </div>
                                 </div>
-                                <div className="space-height-10h"></div>
-                                <div className="space-height-10h"></div>
-                                <div className="space-height-10h"></div>
-                                <div className="space-height-10h"></div>
+                                <div className="space-10h"></div>
+                                <div className="space-10h"></div>
+                                <div className="space-10h"></div>
+                                <div className="space-10h"></div>
                                 <div className="row-button">
                                     <div className="button-container">
                                         <img src={clearbutton} alt="clearbutton" />
@@ -77,7 +77,7 @@ const Action = () => {
 
 
                 </div>
-                <div className="space-height"></div>
+                <div className="space-20h"></div>
                 <NormalBanner />
             </div>
 
